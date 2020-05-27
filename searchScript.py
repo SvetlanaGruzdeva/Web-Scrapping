d97f53ac15cf0183a10b936f40cae060faa2ea00
Svetlana <gruzdevasvetlana@gmail.com>
2020-05-27 14:34:09 +0200

    Project 2: done

--- searchScript.py
+++ searchScript.py
@@ -1,23 +1,11 @@
-#! python3
-# searchScript - Opens several search results.
+import webbrowser
+from googlesearch import search
 
-import requests, sys, webbrowser, bs4
-from pprint import pprint
+query = input()
 
-print('Searching...') # display text while downloading the search result page
-# res = requests.get('https://pypi.org/search/?q=' + ' '.join(sys.argv[1:]))
-res = requests.get('https://www.google.com/search?q=' + 'cat')
-# print(res.text)
-res.raise_for_status()
+print('Searching...\n')
+for searchResult in search(query, tld='com', lang='en', num=5, start=0, stop=5, pause=2):
+    print(searchResult)
+    webbrowser.open(searchResult)
 
-# Retrieve top search result links.
-soup = bs4.BeautifulSoup(res.text, 'html.parser') # returns wierd content, probably because of parser. Critically review.
-# print(soup)
-
-# Open a browser tab for each result.
-linkElems = soup.select('p.LC20lb.DKV0Md')
-numOpen = min(5, len(linkElems))
-for i in range(numOpen):
-    urlToOpen = 'https://www.google.com' + linkElems[i].get('href')
-    print('Opening', urlToOpen)
-    webbrowser.open(urlToOpen)
+print('\n' + 'Done!')
